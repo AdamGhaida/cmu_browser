9c240fc6da3f83e3026d3c3a8a24fc9433ec94bc
Adam Abu Ghaida <44762129+AdamGhaida@users.noreply.github.com>
2024-11-23 20:40:41 +0300

    interaction done

--- draw/draw.py
+++ draw/draw.py
@@ -19,7 +19,6 @@ def onAppStart(app):
     app.previousWidth = app.width
     app.previousHeight = app.height
 
-    app.widthProportion = (1400/1710)
     app.webPageXStart = (300/1710)*app.width
     app.webPageYStart = 10
 
@@ -29,7 +28,7 @@ def onAppStart(app):
     app.previousScreenshotPath = "./backend/screenshots/previousWebPage.png"
     
     # Start the background thread for capturing screenshots
-    start_screenshot_thread(app)
+    startScreenshotThread(app)
 
     app.inputWord = ""
 
@@ -37,38 +36,35 @@ def onAppStart(app):
 
 def redrawAll(app):
     if app.isScreenshotReady:
-        # If the screenshot is ready, draw it
+        
         if not app.screenshotQueue.empty():
             screenshot_path = app.screenshotQueue.get()
             
-            # Check if the screenshot file exists before drawing it
+            
             if os.path.exists(screenshot_path):
                 drawImage(screenshot_path, app.webPageXStart, app.webPageYStart)
         
-        # Set the flag back to False to indicate we are drawing the current image
+        
         app.isScreenshotReady = False
     else:
-        # If the screenshot isn't ready, draw the previous one
         if os.path.exists(app.previousScreenshotPath):
             drawImage(app.previousScreenshotPath, app.webPageXStart, app.webPageYStart)
 
 
-# Function to start the screenshot capture thread (only once)
-def start_screenshot_thread(app):
+def startScreenshotThread(app):
     if not hasattr(app, 'screenshotThread'):
         app.screenshotThread = threading.Thread(target=app.sel.updatePage, args=(app.screenshotQueue,))
         app.screenshotThread.daemon = True  # Allow the thread to exit when the program exits
         app.screenshotThread.start()
 
 
-# CMU Graphics `onStep()` function (called 30 times per second)
 def onStep(app):
-    start_screenshot_thread(app)
+    startScreenshotThread(app)
 
     if app.width != app.previousWidth or app.height != app.previousHeight:
         print(f"Window size changed to: {app.width}x{app.height}")
 
-        app.sel.updateWindowSize(app.widthProportion*app.width,app.height-20)
+        app.sel.updateWindowSize(app.width-app.webPageXStart-10,app.height-20)
         # Update previous dimensions
         app.previousWidth = app.width
         app.previousHeight = app.height
@@ -92,6 +88,6 @@ def onKeyPress(app, key):
 def onMousePress(app,x,y):
     if app.webPageXStart<x<app.width-10 and 0<y<app.height-10:
         print("clicked!!!", x,y)
-        app.sel.click(x-330,y-10)
+        app.sel.click(x-app.webPageXStart,y-10)
 
 runApp(width=1710,height=920)
